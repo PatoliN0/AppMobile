cf6576e9c7296aaab62d6f6f08ccf589c08adb48
Felipe Miranda Soares <felipe.soares@4strats.com>
2019-07-31 15:49:33 -0300

    Adicao de novos componentes e um feed legal

---
+++ src/app/feed/feed.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.page.html',
+  styleUrls: ['./feed.page.scss'],
+})
+export class FeedPage implements OnInit {
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
